9d2318500ee56af33bbbda2b915dae79871d0a73
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-20 13:20:17 +0100

    add jumbotron section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
-export default function Home(){
+import { NavLink } from "react-router-dom";
 
-    return(
-        <main>
-            Home page
-        </main>
-    )
+export default function Home() {
+  return (
+    <main>
+      <section className="jumbo_img d-flex align-items-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h1 className="fw-bold text-white">The Everything eCommerce</h1>
+            </div>
+            <div className="col pt-3">
+              <NavLink to="/products" className="btn btn-primary">
+                Products
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
