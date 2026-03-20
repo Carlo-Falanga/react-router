653c731d44482e28acbfa43f81ce49afcad59252
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-20 17:49:00 +0100

    add subtitle to home page jumbo title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,8 @@ export default function Home() {
         <div className="container">
           <div className="row">
             <div className="col-12">
-              <h1 className="fw-bold">Flanagan eCommerce</h1>
+              <h1 className="fw-bold">Flanagan e-Commerce</h1>
+              <p className="text-secondary-emphasis">Simple products. Smart choices. Better living.</p>
             </div>
             <div className="col pt-3">
               <NavLink to="/products" className="btn btn-primary rounded-pill mx-3 px-4 py-2 fw-medium">
