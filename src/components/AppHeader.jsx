8f71adbf6f8762b4883c229ec726f0a76dcdae1a
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-20 17:22:04 +0100

    general fixes

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -22,16 +22,16 @@ export default function AppHeader() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink className="nav-link px-4 fw-medium" to="/">
+                <NavLink className="nav-link px-4" to="/">
                   Home
                 </NavLink>
               </li>
-              <li className="nav-item fw-medium">
+              <li className="nav-item">
                 <NavLink className="nav-link px-4" to="/products">
                   Products
                 </NavLink>
               </li>
-              <li className="nav-item fw-medium">
+              <li className="nav-item">
                 <NavLink className="nav-link px-4" to="/aboutus">
                   About Us
                 </NavLink>
