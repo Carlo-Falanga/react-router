d0856b56bd4d2a380f591c9dd7fcec6cbb47d906
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-20 13:00:15 +0100

    add font dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import * as bootstrap from 'bootstrap'
+// Supports weights 100-900
+import '@fontsource-variable/inter';
 import './index.css'
 import App from './App.jsx'
 
