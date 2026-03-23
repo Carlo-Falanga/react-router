2eff968e9eed3e907f2fedd9895b5cac6b9347df
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-23 12:43:34 +0100

    style: enhance Product and SingleProduct components with improved layout and button styling

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import  axios  from "axios";
+import { Link } from "react-router-dom";
 
 export default function SingleProduct() {
   const [product, setProduct] = useState([]);
@@ -22,10 +23,20 @@ export default function SingleProduct() {
 
   return <>
     <section>
-        <div className="row">
+        <div className="container">
+
+        <div className="row row-cols-1 row-cols-lg-2 text-center text-lg-start g-5 my-5 py-5">
+            <div className="col">
+                <img className="w-75" src={product.image} alt="" />
+            </div>
             <div className="col">
                 <h3>{product.title}</h3>
+                <span className="fw-bold">{product.price} $</span>
+                <p className="py-4">{product.description}</p>
+                <Link className='btn btn-primary px-5 py-2' to={'/products'}>Add to cart</Link>
             </div>
+        </div>
+
         </div>
     </section>
   </>;
