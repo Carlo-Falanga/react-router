6b8feebc948edae42ea358872b4f90392ad9fdea
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-23 12:15:47 +0100

    fetch product details in SingleProduct component

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,8 +1,32 @@
-export default function SingleProduct(){
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import  axios  from "axios";
 
-    return(
-        <>
+export default function SingleProduct() {
+  const [product, setProduct] = useState([]);
+  const { id } = useParams();
+
+  const api_url = `https://fakestoreapi.com/products/${id}`;
+
+  useEffect(() =>{
+    axios.get(api_url)
+    .then(data =>{
+        setProduct(data.data)
         
-        </>
-    )
+    })
+  }, [])
+
+  console.log(product);
+  
+
+  return <>
+    <section>
+        <div className="row">
+            <div className="col">
+                <h3>{product.title}</h3>
+            </div>
+        </div>
+    </section>
+  </>;
 }
